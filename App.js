0b3cbece43c7fdaf58f758e5a4544d737a9c2f0f
Minhaj Islam <minhajislam674@gmail.com>
2023-04-01 15:00:33 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  SafeAreaView,
+  Button,
+  Alert,
+  Platform,
+  StatusBar,
+  Dimensions,
+  View,
+} from "react-native";
+
+import {
+  useImageDimensions,
+  useDeviceOrientation,
+} from "@react-native-community/hooks";
+import WelcomeScreen from "./app/assets/screens/WelcomeScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const { landscape } = useDeviceOrientation();
+
+  return <WelcomeScreen />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "grey",
+
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
---
+++ app/assets/screens/WelcomeScreen.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {
+  Button,
+  ImageBackground,
+  StyleSheet,
+  View,
+  Button,
+} from "react-native";
+
+const WelcomeScreen = () => {
+  return (
+    <ImageBackground>
+      <View></View>
+    </ImageBackground>
+  );
+};
+
+export default WelcomeScreen;
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+});
